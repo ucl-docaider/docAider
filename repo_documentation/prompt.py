1356515c4cd8aeb06ddc5601be863efe975d61ed
Dias2406 <ucabdj5@ucl.ac.uk>
2024-06-24 20:42:11 +0100

    [documentation] added file level documentation
    fixed the file content being empty

--- repo_documentation/prompt.py
+++ repo_documentation/prompt.py
@@ -6,53 +6,87 @@ Now you need to generate a document for "{file_name}".
 The content of the code is as follows:
 {file_content}
 
-The standard format is as follows:
+Please note any part of the content you generate SHOULD NOT CONTAIN Markdown hierarchical heading and divider syntax.
 
-# {file_name}: 
+The standard format is as follows (If a section does not have any information, you can skip it and move to the next one):
 
-The function of {file_name} is XXX. (Only code name and one sentence function description are required)
+# {file_name}
 
-## Class 1: NameOfClass1
+## ClassDef NameOfClass
 
-### Attributes
+The function of the class is XXX. (Only code name and one sentence function description are required)
+
+**Attributes**:
 
 - `attribute1` (`type`): Description of the first attribute.
-- `attribute2` (`type`): Description of the second attribute.
 
-### Functions 
+**Functions**:
 
-- `function_name`(param1: type, param2: type) -> return_type
+- `function_name1`(`param1`: `type`) -> `return_type`
     - Parameters:
         - `param1` (`type`): Description of the first parameter.
-        - `param2` (`type`): Description of the second parameter.
-        - Returns:
-            - `return_type`: Description of the return value.
-- Called_functions: List of functions called within this function and short description of what they do.
-        - `function1`: Description of what function1 does.
-        - `function2`: Description of what function2 does.   
+    - Returns:
+        - `return_type`: Description of the return value.
 
-## Function 1: NameOfFunction1: (functions that do not belong to a class but are still present in the file)
+**Called_functions**:
 
-### Parameters:
+- `function1`(`param1`: `type`) -> `return_type`: Description of what function1 does and what function1 returns.
+
+**Code Description**: The description of this class. (Detailed and CERTAIN code analysis and description)
+
+**Note**: Points to note about the use of the code according to the returns
+
+**Input Example**: 
+
+```
+Provide an input example for a specified data type (e.g., list, double, int) and include a detailed explanation.
+```
+
+**Output Example**:
+
+```
+Provide an output example for a specified data type (e.g., list, double, int) and include a detailed explanation.
+```
+
+
+
+## FunctionDef NameOfFunction (functions that do not belong to a class but are still present in the file)
+
+The function of the function is XXX. (Only code name and one sentence function description are required)
+
+**Parameters**:
 
 - `param1` (`type`): Description of the first parameter.
-- `param2` (`type)`: Description of the second parameter.
 
-### Returns:
+**Returns**:
 
 - `return_type`: Description of the return value.
 
-### Called_functions: List of functions called within this function and a short description of what they do.
+**Called_functions**:
 
-- `function1`: Description of what function1 does.
-- `function2`: Description of what function2 does.
+- `function1`(`param1`: `type`) -> `return_type`: Description of what function1 does and what function1 returns.
 
-## Note: Consider the additional documentation for the functions and classes called within the file:
+**Code Description**: The description of this function. (Detailed and CERTAIN code analysis and description)
+
+**Note**: 
+- Please generate a detailed explanation document for this object based on the code of the target object itself. For the section Called_functions, considering the additional documentation for the functions and classes called within the file:
+
 {additional_docs}
 
-Please note:
-- Any part of the content you generate SHOULD NOT CONTAIN Markdown hierarchical heading and divider syntax.
+- Points to note about the use of the code according to the returns
+
+**Input Example**: 
+
+```
+Provide an input example for a specified data type (e.g., list, double, int) and include a detailed explanation.
+```
+
+**Output Example**: 
+
+```
+Provide an output example for a specified data type (e.g., list, double, int) and include a detailed explanation.
+```
 
 """
 
-USR_PROMPT = """You are a documentation generation assistant for Python programs. Keep in mind that your audience is document readers, so use a deterministic tone to generate precise content and don't let them know you're provided with code snippet and documents. AVOID ANY SPECULATION and inaccurate descriptions! Now, provide the documentation for the target object in a professional way."""
+USR_PROMPT = """Keep in mind that your audience is document readers, so use a deterministic tone to generate precise content and don't let them know you're provided with code snippet and documents. AVOID ANY SPECULATION and inaccurate descriptions! Now, provide the documentation for the target object in python language in a professional way."""
