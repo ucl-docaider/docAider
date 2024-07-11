656daa237c57398f1961de252c1a9e118aa517f7
tanmaythaware <tanmaythaware123@gmail.com>
2024-07-11 14:31:18 +0100

    changed llama config to gpt-4
    update gitignore

--- autogen_utils/config.py
+++ autogen_utils/config.py
@@ -1,5 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the variables from the environment
 llm_config = dict(
-    model="llama3",
-    base_url="http://localhost:11434/v1",
-    api_key="ollama",
+    model=os.getenv("MODEL"),
+    base_url=os.getenv("BASE_URL"),
+    api_key=os.getenv("API_KEY"),
+    api_type=os.getenv("API_TYPE"),
+    api_version=os.getenv("API_VERSION"),
 )
