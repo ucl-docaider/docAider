0cc17e91b2ee74f53298ba22c79758e6ae762bd4
TomasKopunec <tomas.kopunec5@gmail.com>
2024-07-20 21:02:08 +0100

    Front End Improvements
    Sidebar
    
    Buttons added
    
    Copy button
    
    Sort by alphabet
    
    Cleanup
    
    Added table of content support
    
    Added selection animation
    
    Improvements
    
    Added close and copy buttons

--- repo_documentation/merging/merger.py
+++ repo_documentation/merging/merger.py
@@ -3,6 +3,18 @@ from datetime import datetime
 
 EXTENSION = '.html'
 
+with open('repo_documentation/merging/head.html', 'r', encoding='utf-8') as f:
+    head = f.read()
+
+with open('repo_documentation/merging/body.html', 'r', encoding='utf-8') as f:
+    body = f.read()
+
+with open('repo_documentation/merging/file-card.html', 'r', encoding='utf-8') as f:
+    file_card_template = f.read()
+
+with open('repo_documentation/merging/script.html', 'r', encoding='utf-8') as f:
+    script = f.read()
+
 
 class TemplateDto:
     def __init__(self, project_title, repo_url, project_description, report_bug_url, feature_request_url):
@@ -14,42 +26,28 @@ class TemplateDto:
 
 
 def create_documentation(docs_folder, dto: TemplateDto):
-    head_path = 'repo_documentation/merging/head.html'
-    body_path = 'repo_documentation/merging/body.html'
-
     # Get the current date and time
     current_datetime = datetime.now().strftime("%d/%m/%Y %H:%M")
 
-    # Read the body
-    with open(head_path, 'r', encoding='utf-8') as f:
-        head = f.read()
-    with open(body_path, 'r', encoding='utf-8') as f:
-        body = f.read()
-
     # Generate table of contents
-    table_of_contents = ""
-    for root, _, files in os.walk(docs_folder):
-        for file in files:
+    files = []
+    for root, _, _files in os.walk(docs_folder):
+        for file in _files:
             if file.endswith(EXTENSION) and not file.startswith('index'):
-                rel_path = os.path.relpath(
-                    os.path.join(root, file), docs_folder)
-                link = rel_path.replace('\\', '/').replace(EXTENSION, '')
-                id = link.replace('/', '-').replace('.', '-')
-                table_of_contents += f'<li><a href="#{id}">{link}</a></li>\n'
+                path = os.path.relpath(os.path.join(root, file), docs_folder)
+                files.append((root, path))
 
-    # Generate documentation content
-    documentation_content = ""
-    for root, _, files in os.walk(docs_folder):
-        for file in files:
-            if file.endswith(EXTENSION) and not file.startswith('index'):
-                rel_path = os.path.relpath(
-                    os.path.join(root, file), docs_folder)
-                link = rel_path.replace('\\', '/').replace(EXTENSION, '')
-                with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
-                    documentation_content += create_file_card(link, f.read())
+    # Sort by basename
+    files.sort(key=lambda x: os.path.basename(x[1]))
 
-      # Replace placeholders in the template
-    script = get_script()
+    # Get table of contents
+    tree = to_tree([path for _, path in files])
+    table_of_contents = get_table_of_contents(tree)
+
+    # Get the documentation file-cards
+    documentation_content = get_documentation_content(files)
+
+    # Replace placeholders in the template
     filled_template = body.format(
         project_title=dto.project_title,
         repo_url=dto.repo_url,
@@ -72,47 +70,90 @@ def create_documentation(docs_folder, dto: TemplateDto):
 
 
 def create_file_card(file_path: str, docs):
-    id = file_path.replace('/', '-').replace('.', '-')
-    return f"""
-<div class="file-card">
-    <details>
-        <summary id="{id}">{file_path}</summary>
-        <div class="content">
-            {docs}
-        </div>
-    </details>
-</div>
-"""
+    id = clean_path(file_path)
+    file_name = file_path.replace('\\', '/').replace('.html', '')
+    return file_card_template.format(id=id,
+                                     file_name=file_name,
+                                     content=docs)
 
 
-def get_script():
-    return """
-<script>
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-        });
-</script>
-"""
+def get_table_of_contents(tree, prefix=""):
+    table_of_contents = "<ul>\n"
+
+    # Separate directories and files
+    directories = []
+    files = []
+    for key, value in tree.items():
+        if key == 'files':
+            files.extend(value)
+        elif isinstance(value, dict):
+            directories.append((key, value))
+        else:
+            files.append(key)
+
+    # Sort directories alphabetically
+    directories.sort(key=lambda x: x[0].lower())
+
+    # Sort files alphabetically
+    files.sort(key=str.lower)
+
+    # Handle directories first
+    for key, value in directories:
+        table_of_contents += f'<li><details><summary>📁 {key}</summary>\n'
+        table_of_contents += get_table_of_contents(value, prefix + key + "/")
+        table_of_contents += '</details></li>\n'
+
+    # Then handle files
+    for file in files:
+        id = clean_path(prefix + file)
+        link = os.path.basename(file).replace(EXTENSION, '')
+        table_of_contents += f'<li><a href="javascript:void(0);" onclick="showFile(\'{id}\')">🐍 {link}</a></li>\n'
+
+
+    table_of_contents += "</ul>\n"
+    return table_of_contents
+
+
+def clean_path(path):
+    return path.replace(EXTENSION, '') \
+        .replace('\\', '/') \
+        .replace('/', '-') \
+        .replace('.', '-')
+
+
+def get_documentation_content(files):
+    documentation_content = ""
+    for root, path in files:
+        basename = os.path.basename(path)
+        with open(os.path.join(root, basename), 'r', encoding='utf-8') as f:
+            documentation_content += create_file_card(path, f.read())
+    return documentation_content
+
+
+def to_tree(files):
+    tree = {}
+    for path in files:
+        parts = path.split('\\')
+        current = tree
+        for part in parts[:-1]:
+            if part not in current:
+                current[part] = {}
+            current = current[part]
+        if isinstance(current, dict):
+            if 'files' not in current:
+                current['files'] = []
+            current['files'].append(parts[-1])
+        else:
+            current.append(parts[-1])
+    return tree
 
 
 """
 TODO
-- Remove the files section (table of contents) add it as a sidebar, so it's easier to navigate
-- Add fold all and unfold all buttons that scrolls with the page
-- Add a 'Scroll to top' button that scrolls with the page
 - Add a search bar (file_name)
 - Add created with 'Repo-Copilot' in the footer (link to repo)
 
 Hard TODO
 - Add dark and light mode
-- Add a button to copy the documentation to the clipboard
-- Add a button to download the documentation as a PDF
 - Folow the tree structure of the files (folder/file)
-- 
-
 """
